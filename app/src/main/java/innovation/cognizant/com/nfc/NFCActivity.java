d6cc74f63d14e840754848ac52ae801b45e3f080
Poorna <poorna.nimmala@gmail.com>
2018-06-01 23:37:17 -0500

    -Showing Received Payload on LinearLayout -LaunchMode changed to SingleTask to avoid creating multiple instances

--- app/src/main/java/innovation/cognizant/com/nfc/NFCActivity.java
+++ app/src/main/java/innovation/cognizant/com/nfc/NFCActivity.java
@@ -13,8 +13,10 @@ import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,8 +27,8 @@ public class NFCActivity extends AppCompatActivity implements
 
     private static final String TAG = "NFCActivity" ;
     EditText mInputView;
-    Button sendButton;
     private NfcAdapter mNfcAdapter;
+    private LinearLayout receivedDataLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,17 +39,8 @@ public class NFCActivity extends AppCompatActivity implements
 
     private void init() {
         mInputView = (EditText) findViewById(R.id.input_text);
-        sendButton = (Button) findViewById(R.id.send_btn);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        receivedDataLayout = (LinearLayout) findViewById(R.id.data_layout);
         initNfcAdapter();
-
         if (getIntent().getAction().equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
             handleNfcIntent(getIntent());
         }
@@ -60,14 +53,33 @@ public class NFCActivity extends AppCompatActivity implements
             if (receivedPayload!=null) {
                 NdefMessage receivedMessage = (NdefMessage) receivedPayload[0];
                 NdefRecord[] receivedRecord = receivedMessage.getRecords();
+                Log.i(TAG, "receivedRecord Size::"+receivedRecord.length);
                 byte[] decodedBytes = Base64.decode(receivedRecord[0].getPayload(),Base64.DEFAULT);
                 String payload = new String(decodedBytes);
                 Log.i(TAG,"Received payload::"+payload );
                 Toast.makeText(this, "Received Data::: "+payload, Toast.LENGTH_LONG).show();
+                attachDataToLayout(payload);
                 //TODO: Need to reset intent?? to handle orientation??
             }
         }
 
+    }
+
+    private void attachDataToLayout(final String payload) {
+
+        Log.i(TAG,"is receivedDataLayout NULL??::"+receivedDataLayout );
+        if(receivedDataLayout == null) {
+            return;
+        }
+
+        TextView textview = new TextView(this);
+        textview.setText(payload);
+        LinearLayout.LayoutParams textLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        textLayoutParams.setMargins(10, 0,0,10);
+        textview.setLayoutParams(textLayoutParams);
+        receivedDataLayout.addView(textview);
+        Log.i(TAG,"receivedDataLayout ChildCount??::"+receivedDataLayout.getChildCount() );
     }
 
     @Override
@@ -99,8 +111,20 @@ public class NFCActivity extends AppCompatActivity implements
             return null;
         }
 
-        NdefRecord ndfRecord = createNdefRecord(textToSend);
-        return new NdefMessage(ndfRecord);
+       // NdefRecord ndfRecord = createNdefRecord(textToSend);
+        //NdefRecord record = null;
+
+        byte[] data = textToSend.getBytes(Charset.defaultCharset());
+        byte[] base64Payload = Base64.encode(data, Base64.DEFAULT);
+
+        NdefMessage msg = new NdefMessage(
+                new NdefRecord[] { NdefRecord.createMime(
+                        "application/"+getPackageName(), base64Payload)
+                        ,NdefRecord.createApplicationRecord(getPackageName())});
+
+        //NdefRecord.createApplicationRecord("innovation.cognizant.com.nfc")
+
+        return msg;
     }
 
     private NdefRecord createNdefRecord(String textToSend) {
@@ -127,7 +151,11 @@ public class NFCActivity extends AppCompatActivity implements
         Log.i(TAG, "Message sent successfully::");
         if(mInputView!=null) {
             //reset input box
-            mInputView.setText("");
+            try{
+                mInputView.setText("");
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
         }
     }
 }
